bd276550fbbbe3f5806136e00c01279af75e9818
Abhinav piratla <35750996-piratlaabhinav2@users.noreply.replit.com>
2024-08-12 17:00:15 +0000

    Combined App and PasswordChecker components, updated structure

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
+  base: '/password-strength-checker/',
+  build: {
+    outDir: 'dist',
   },
-  server: {
-    host: '0.0.0.0',
-  },
-});
+})
